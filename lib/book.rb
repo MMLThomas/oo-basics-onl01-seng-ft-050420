e084caea0d3b1aecc443a983431a94da713776e9
michael thomas <michael.thomas2014@gmail.com>
2020-05-14 06:40:52 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,11 +3,8 @@ class Book
   def initialize(title)
     @title = title
   end
-  
-  def title
-    @title
-  end
-  
+
+  attr_reader :title
   attr_accessor :author, :page_count, :genre 
   
   def turn_page
